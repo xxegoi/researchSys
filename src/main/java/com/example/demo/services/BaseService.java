70a6bb4e2a366a00ed4a753d459a4045aed4170e
xxegoi <250242742@qq.com>
2018-12-28 11:03:20 +0800

    Revert "AAA"
    This reverts commit 8577dd053dafde3fff591ad406ca7e4b3c2d28e4.

--- src/main/java/com/example/demo/services/BaseService.java
+++ src/main/java/com/example/demo/services/BaseService.java
@@ -1,4 +1,22 @@
 package com.example.demo.services;
 
+<<<<<<< HEAD:src/main/java/com/example/demo/services/ResultService.java
+import com.example.demo.pojo.Result;
+
+import java.util.List;
+
+public interface ResultService {
+
+    Result GetResult(String user, int questionId,int researchId);
+
+    int insert(Result result);
+
+    int update(Result result);
+
+    int delete(int Id);
+
+    List<Result> getAllByResearchIdAndUser(String user, int researchId);
+=======
 public class BaseService {
+>>>>>>> parent of c074f7b... Test OK:src/main/java/com/example/demo/services/BaseService.java
 }
--- src/main/java/com/example/demo/services/Impl/ResearchService.java
+++ src/main/java/com/example/demo/services/Impl/ResearchService.java
@@ -1,4 +1,58 @@
 package com.example.demo.services.Impl;
 
+<<<<<<< HEAD:src/main/java/com/example/demo/services/Impl/AnswerServiceImpl.java
+import com.example.demo.dao.AnswerDao;
+import com.example.demo.pojo.Answer;
+import com.example.demo.services.AnswerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+@Service
+public class AnswerServiceImpl implements AnswerService {
+
+    @Autowired
+    AnswerDao answerDao=null;
+
+    @Override
+    public List<Answer> GetList(int questionId) {
+        return answerDao.getList(questionId);
+    }
+
+    @Override
+    @Transactional(propagation = Propagation.REQUIRED,isolation = Isolation.READ_COMMITTED)
+    public int insert(Answer answer) {
+        return answerDao.insert(answer);
+    }
+
+    @Override
+    @Transactional(propagation = Propagation.REQUIRED,isolation = Isolation.READ_COMMITTED)
+    public int update(Answer answer) {
+        return answerDao.update(answer);
+    }
+
+    @Override
+    @Transactional(propagation = Propagation.REQUIRED,isolation = Isolation.READ_COMMITTED)
+    public int delete(int Id) {
+        return answerDao.delete(Id);
+    }
+
+    @Override
+    @Transactional(propagation = Propagation.REQUIRED,isolation = Isolation.READ_COMMITTED)
+    public int deleteByQuestionId(int questionId) {
+        return answerDao.deleteByQuestionId(questionId);
+    }
+
+    @Override
+    @Transactional(propagation = Propagation.REQUIRES_NEW,isolation = Isolation.READ_COMMITTED)
+    public int insertList(List<Answer> answers) {
+        return answerDao.insertList(answers);
+    }
+
+=======
 public class ResearchService {
+>>>>>>> parent of c074f7b... Test OK:src/main/java/com/example/demo/services/Impl/ResearchService.java
 }
